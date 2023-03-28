83637066a022e5c263114f3338cf22b6b6817eeb
EzequielDiez <ezequieldiez.emd@gmail.com>
2023-03-27 23:55:04 -0300

    Desafío 3 - Servidor con Express

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import ProductManager from './ProductManager.js';
+
+const app = express()
+
+const productManager = new ProductManager();
+
+    app.get('/products', (req, res) => {
+        const limit = req.query.limit;
+        const products = productManager.getProducts(limit);
+        res.json({ products });
+    });
+
+    app.get('/products/:pid', (req, res) => {
+        const productId = req.params.pid;
+        const product = productManager.getProductById(productId);
+        res.json(product);
+    });
+  
+app.listen(8080, () => {
+    console.log('Server started on port 8080');
+});
